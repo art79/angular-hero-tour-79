147772e143265211555ac8c597e6e12b9bd52ede
art79 <artm@cae.com>
2020-02-15 08:31:17 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'; // Import Angular's Decorator
 
 // A component controls a patch of screen called a view
 @Component({
+  selector: 'heroes-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
